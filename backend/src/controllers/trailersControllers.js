1b8cd5c68dfd646733888267e30444a5ebf4c429
Hoang <taovohoang2k6@gmail.com>
2026-06-19 11:34:47 +0700

    update-delete-Trailer

--- backend/src/controllers/trailersControllers.js
+++ backend/src/controllers/trailersControllers.js
@@ -82,3 +82,94 @@ export const createTrailer = async (req, res) => {
     });
   }
 };
+
+export const updateTrailer = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie_id, title, youtube_url, thumbnail, status } = req.body;
+
+    const trailer = await Trailer.findById(id);
+
+    if (!trailer) {
+      return res.status(404).json({
+        success: false,
+        message: "Không tìm thấy trailer",
+      });
+    }
+
+    if (movie_id) {
+      const movie = await Movie.findOne({
+        _id: movie_id,
+        deleted_at: null,
+      });
+
+      if (!movie) {
+        return res.status(404).json({
+          success: false,
+          message: "Không tìm thấy phim",
+        });
+      }
+
+      trailer.movie_id = movie_id;
+    }
+
+    if (title !== undefined) {
+      trailer.title = title;
+    }
+
+    if (youtube_url !== undefined) {
+      trailer.youtube_url = youtube_url;
+    }
+
+    if (thumbnail !== undefined) {
+      trailer.thumbnail = thumbnail;
+    }
+
+    if (status !== undefined) {
+      trailer.status = status;
+    }
+
+    await trailer.save();
+
+    const populatedTrailer = await Trailer.findById(trailer._id).populate(
+      "movie_id",
+      "title poster banner status release_date"
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Cập nhật trailer thành công",
+      data: populatedTrailer,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const deleteTrailer = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const trailer = await Trailer.findByIdAndDelete(id);
+
+    if (!trailer) {
+      return res.status(404).json({
+        success: false,
+        message: "Không tìm thấy trailer",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Xóa trailer thành công",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/src/router/trailersRouters.js
+++ backend/src/router/trailersRouters.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createTrailer, getAllTrailers } from "../controllers/trailersControllers.js";
+import { createTrailer, deleteTrailer, getAllTrailers, updateTrailer } from "../controllers/trailersControllers.js";
 
 const router = express.Router();
 
 router.get("/", getAllTrailers);
 router.post("/", createTrailer);
+router.put("/:id", updateTrailer);
+router.delete("/:id", deleteTrailer);
 
 export default router;
