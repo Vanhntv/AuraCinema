431eaaed15a24ca90d2ae5e4b8b53ee2ddb2473e
chamnguyenhuyen-ui <chamnguyenhuyen@gmail.com>
2026-07-16 16:51:57 +0700

    bỏ Liên hoan phim. tuần phim

--- frontend-user/src/data/homeData.js
+++ frontend-user/src/data/homeData.js
@@ -4,7 +4,6 @@ export const navItems = [
   { name: 'Tin tức', path: '/tin-tuc' },
   { name: 'Khuyến mãi', path: '/khuyen-mai' },
   { name: 'Giá vé', path: '/gia-ve' },
-  { name: 'Liên hoan phim, Tuần phim', path: '/lien-hoan-phim' },
   { name: 'Giới thiệu', path: '/gioi-thieu' }
 ];
 
